7ec5be62362c047cf2b9cb357b6a6e7eb6a91e3b
Gina Sprint <sprint@gonzaga.edu>
2021-10-26 12:43:50 -0700

    initial

---
+++ app/src/main/java/com/sprint/gina/recyclerviewfun/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sprint.gina.recyclerviewfun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
